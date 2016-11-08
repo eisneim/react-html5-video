e80e5d656dd96333864dd6d916aa38d2d40f1f5f
eisneim <eisneim1@gmail.com>
2016-11-08 14:58:11 +0800

    version 1.0.3 for bug fix

--- demo/staticfile_server.js
+++ demo/staticfile_server.js
@@ -4,10 +4,11 @@ var http = require('http'),
 	fs = require('fs');
 
 // url :  /static/file.html
-/* 
+/*
 	将获取的二进制数据写入另一个文件
 	curl -o outpu.jpg http://localhost:8888/static/test.jpg
- */ 
+ */
+var port = 8888;
 http.createServer(function(req,res){
 
 	req.parsed_url = url.parse(req.url,true);
@@ -19,10 +20,10 @@ http.createServer(function(req,res){
 	// 	res.writeHead(404,{'Content-Type':'text/plain'});
 	// 	res.end('not found');
 	// }
+	console.log('server listen at ', port)
 
 
-
-}).listen(8888);
+}).listen(port);
 
 function serveStatic(req,res){
 
@@ -50,16 +51,16 @@ function getContentType(filename){
 	var ext = path.extname(filename).toLowerCase();
 
 	switch(ext){
-		case '.mp4':  
+		case '.mp4':
 			return 'video/mp4'
-		case '.webm':  
+		case '.webm':
 			return 'video/webm'
-		case '.ogv':  
+		case '.ogv':
 			return 'video/ogv'
-		case '.jpg': case '.jpeg': 
+		case '.jpg': case '.jpeg':
 			return 'image/jpeg'
 		case '.png':
-			return 'image/png'; 
+			return 'image/png';
 		case '.gif':
 			return 'image/gif';
 		case '.html':
